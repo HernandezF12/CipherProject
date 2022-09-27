a47d5a033ae0f53621a496ae7762d92802c8504a
Felipe Hernandez <hernandezfjr15@gmail.com>
2022-09-27 09:34:01 -0500

    added some testing to the test files for C and added the alphabet array to S file

--- src/substitution.js
+++ src/substitution.js
@@ -7,6 +7,34 @@ const substitutionModule = (function () {
   // you can add any code you want within this function scope
 
   //add the substitution alphabet given from qualified
+  const alphabet = {
+    a:x,
+    b:o,
+    c:y,
+    d:q,
+    e:m,
+    f:c,
+    g:g,
+    h:r, 
+    i:u,
+    j:k,
+    k:s,
+    l:w,
+    m:a,
+    n:f,
+    o:l,
+    p:n,
+    q:t,
+    r:h,
+    s:d,
+    t:j,
+    u:p,
+    v:z,
+    w:i,
+    x:b,
+    y:e,
+    z:v
+  }
 
   function substitution(input, alphabet, encode = true) {
     // your solution code here
